f1027925ad990329034b6855e68aca16f711fad5
Alex Bazlinton <abazlinton@gmail.com>
2021-12-18 19:02:28 +0000

    back to all domains

--- 3letter.js
+++ 3letter.js
@@ -43,7 +43,7 @@ function sleep(time) {
 
   // stop the progress bar
   let numberofResults = 0
-  const promises = domains.slice.map(async (domain, index) => {
+  const promises = domains.map(async (domain, index) => {
     await sleep(index * 20);
     bar.update(++numberofResults);
     return await ensContract.available(domain)
