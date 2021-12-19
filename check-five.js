cc49b34fc1695a5bdd1a6c494c30f70485522660
Alex Bazlinton <abazlinton@gmail.com>
2021-12-19 12:57:21 +0000

    sleep missing

--- check-five.js
+++ check-five.js
@@ -11,28 +11,32 @@ const ensContractAbi = ["function available(string name) view returns (bool)"]
 const provider = new ethers.providers.JsonRpcProvider(nodeHttpUrl);
 const ensContract = new ethers.Contract(ensAddress, ensContractAbi, provider)
 
-  ; (async () => {
+function sleep(time) {
+  return new Promise((resolve) => setTimeout(resolve, time))
+}
 
-    const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-    bar.start(fiveLetterWords.length, 0);
-    let numberofResults = 0
-    const promises = fiveLetterWords.slice(0, 100).map(async (domain, index) => {
-      await sleep(index * 5);
-      bar.update(++numberofResults);
-      return await ensContract.available(domain)
-        .then(res => ({ domain, isAvailable: res, didFail: false }))
-        .catch(err => {
-          console.log(err)
-          return { domain, isAvailable: false, didFail: true }
-        })
-    })
+; (async () => {
 
-    const results = await Promise.all(promises)
-    const fails = results.filter(res => res.didFail)
-    console.log("fails:", fails.length)
-    const responses = results.filter(res => !res.didFail)
-    const onlyAvailableDomains = responses.filter(res => res.isAvailable)
-    const onlyDomainNames = onlyAvailableDomains.map(d => d.domain)
-    console.dir(onlyDomainNames, { maxArrayLength: null })
-    fs.writeFileSync("./freefive.json", JSON.stringify(onlyDomainNames))
-  })()
+  const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+  bar.start(fiveLetterWords.length, 0);
+  let numberofResults = 0
+  const promises = fiveLetterWords.slice(0, 100).map(async (domain, index) => {
+    await sleep(index * 5);
+    bar.update(++numberofResults);
+    return await ensContract.available(domain)
+      .then(res => ({ domain, isAvailable: res, didFail: false }))
+      .catch(err => {
+        console.log(err)
+        return { domain, isAvailable: false, didFail: true }
+      })
+  })
+
+  const results = await Promise.all(promises)
+  const fails = results.filter(res => res.didFail)
+  console.log("fails:", fails.length)
+  const responses = results.filter(res => !res.didFail)
+  const onlyAvailableDomains = responses.filter(res => res.isAvailable)
+  const onlyDomainNames = onlyAvailableDomains.map(d => d.domain)
+  console.dir(onlyDomainNames, { maxArrayLength: null })
+  fs.writeFileSync("./freefive.json", JSON.stringify(onlyDomainNames))
+})()
