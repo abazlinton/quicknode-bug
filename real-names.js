6fbaacdeabd6abb531add3103904e003afee2a62
Alex Bazlinton <abazlinton@gmail.com>
2021-12-19 12:43:18 +0000

    five letter

--- real-names.js
+++ real-names.js
@@ -4,17 +4,18 @@ const fs = require('fs');
 const onlyDomainNames = require('./results.json')
 const names = fs.readFileSync('./first_names.all.txt').toString().split('\n');
 const words = fs.readFileSync('./words.txt').toString().split('\n');
-const threeLetterNames = names.filter(name => name.length === 3);
-const threeLetterWords = words.filter(word => word.length === 3);
-const allThreeLetterWords = threeLetterNames.concat(threeLetterWords);
-const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-bar.start(onlyDomainNames.length, 0);
-count = 0
-const availableNames = onlyDomainNames.filter(name => {
-  bar.update(++count)
-  return allThreeLetterWords.includes(name)
-})
-bar.stop();
-console.dir(availableNames, { maxArrayLength: null })
+const fiveLetterNames = names.filter(name => name.length === 5);
+const fiveLetterWords = words.filter(word => word.length === 5);
+const allfiveLetterWords = fiveLetterNames.concat(fiveLetterWords);
+// const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+// bar.start(onlyDomainNames.length, 0);
+// count = 0
+// const availableNames = onlyDomainNames.filter(name => {
+//   bar.update(++count)
+//   return allfiveLetterWords.includes(name)
+// })
+// bar.stop();
+// console.dir(availableNames, { maxArrayLength: null })
 
-console.dir(names.filter(name => name.length === 5), { maxArrayLength: null })
+
+console.dir(allfiveLetterWords.length)
